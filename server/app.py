32c64ffa6cf9428031da9996e935a48048063e01
Kim Love <kimberlymlove15@gmail.com>
2023-10-10 00:29:34 -0400

    Build out model and API routes. All tests passing

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from sqlalchemy import desc, asc
 
 from models import db, Message
 
@@ -14,13 +15,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = Message.query.order_by(asc("created_at")).all()
+        messages_dict = [message.to_dict() for message in messages]
 
-@app.route('/messages/<int:id>')
+        return make_response(messages_dict, 200)
+    
+    elif request.method == "POST":
+        new_message = Message(
+            body = request.json["body"],
+            username = request.json["username"]
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        return make_response(new_message.to_dict(), 201)
+
+@app.route('/messages/<int:id>', methods=["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    
+    if request.method == "PATCH":
+        for key in request.json:
+            setattr(message, key, request.json[key])
+        db.session.add(message)
+        db.session.commit()
+
+        return make_response(message.to_dict(), 202)
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        
+        return make_response({}, 204)
 
 if __name__ == '__main__':
     app.run(port=5555)
